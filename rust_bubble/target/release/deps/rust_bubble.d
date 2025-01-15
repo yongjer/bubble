de1600e3d299adf686991861f1d0bbb25a820e29
yongjer <jerjer920813@gmail.com>
2025-01-15 14:23:35 +0900

    移除不必要的編譯產物，新增針對 Rust 專案的構建檔案

---
+++ rust_bubble/target/release/deps/rust_bubble.d
@@ -0,0 +1,5 @@
+/Users/yongjer/程式/bubble/rust_bubble/target/release/deps/librust_bubble.dylib: src/lib.rs
+
+/Users/yongjer/程式/bubble/rust_bubble/target/release/deps/rust_bubble.d: src/lib.rs
+
+src/lib.rs:
---
+++ rust_bubble/target/release/librust_bubble.d
@@ -0,0 +1,1 @@
+/Users/yongjer/程式/bubble/rust_bubble/target/release/librust_bubble.dylib: /Users/yongjer/程式/bubble/rust_bubble/src/lib.rs
