64de800e580c2713f655ef478ec1228900239c91
yongjer <jerjer920813@gmail.com>
2025-01-15 14:36:24 +0900

    重命名 example_function 為 bubble_sort，並在 Python 中新增對 Rust 實作的氣泡排序調用

---
+++ rust_bubble/target/debug/deps/rust_bubble-9430124de8fbf4e6.d
@@ -0,0 +1,5 @@
+/Users/yongjer/程式/bubble/rust_bubble/target/debug/deps/librust_bubble-9430124de8fbf4e6.rmeta: src/lib.rs
+
+/Users/yongjer/程式/bubble/rust_bubble/target/debug/deps/rust_bubble-9430124de8fbf4e6.d: src/lib.rs
+
+src/lib.rs:
--- src/bubble/__init__.py
+++ src/bubble/__init__.py
@@ -1,5 +1,36 @@
 import timeit
 from typing import Sequence
+import ctypes
+
+# 載入.dylib文件
+lib = ctypes.CDLL("lib/librust_bubble.dylib")
+"""
+pub extern "C" fn bubble_sort(arr: *mut f64, len: i32) -> i32 {
+    if arr.is_null() || len <= 0 {
+        return -1;
+    }
+
+    // 將指針轉換為 Rust 切片
+    let slice = unsafe { std::slice::from_raw_parts_mut(arr, len as usize) };
+    
+    // 執行氣泡排序
+    let n = slice.len();
+    for i in 0..n {
+        let mut swapped = false;
+        for j in 0..n - i - 1 {
+            if slice[j] > slice[j + 1] {
+                slice.swap(j, j + 1);
+                swapped = true;
+            }
+        }
+        if !swapped {
+            break;
+        }
+    }
+    
+    0 // 返回0表示成功
+}
+"""
 
 def bubble_sort_by_python3(arr: Sequence[float]) -> list[float]:
     arr = list(arr)  # 轉換為列表
@@ -17,8 +48,13 @@ def bubble_sort_by_python3(arr: Sequence[float]) -> list[float]:
 # TimeIt testing
 if __name__ == "__main__":
     arr = [64, 34, 25, 12, 22, 11, 90]
-    sorted_arr = bubble_sort_by_python3(arr)
-    print("排序後的數組：", sorted_arr)
 
+    # python3
     execution_time = timeit.timeit(lambda: bubble_sort_by_python3([float(x) for x in arr.copy()]), number=1000000)
+    print(f"執行時間: {execution_time} 秒")
+
+    # rust
+    # 將列表轉換為指針
+    arr = (ctypes.c_double * len(arr))(*arr)
+    execution_time = timeit.timeit(lambda: lib.bubble_sort(arr, len(arr)), number=1000000)
     print(f"執行時間: {execution_time} 秒")
